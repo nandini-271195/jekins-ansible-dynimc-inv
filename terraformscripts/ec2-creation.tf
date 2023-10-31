b20b8aac4d04cd8656dfde76151f211f58e47854
nandinipavi <88199087+nandinipavi@users.noreply.github.com>
2023-10-31 05:48:22 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -1,20 +1,13 @@
-
 provider "aws"{
 region = "ap-south-1"
 }
 
-resource "aws_instance" "AWSInstance" {
-     count ="3"
-     ami = "ami-06791f9213cbb608b"
-	 instance_type = "t2.micro"
-	 key_name = "devopsmss"
-	 security_groups = ["launch-wizard-2"]
-	 tags = {
-	 Name = "tomcatserver"
-	 }
+resource "aws_instance" "AWSServer"{
+ami = "ami-05c0f5389589545b7"
+instance_type = "t2.micro"
+key_name = "devopsmt1"						
+security_groups = ["launch-wizard-2"]
+tags = {
+Name = "tomcatserver"
 }
-
-
-
-
-
+}
